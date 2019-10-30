bb1ed453e5a72ce134ef9ffaab5f0c7819d2242d
nql02 <nql02@10.27.19.239>
2019-10-30 16:44:42 +0900

    パッケージ作成

--- flowchart/src/flowchart/no2.java
+++ flowchart/src/flowchart/no2.java
@@ -1,15 +1,9 @@
 package flowchart;
 
-public class no1 {
+public class no2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		int a = 1;
-		int b = 2;
-		int c = a + b;
-
-		c = c + 1;
 
 	}
 
