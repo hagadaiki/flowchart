179c61a00f7529fe99832ed8ed9948f2c7512e6d
nql02 <nql02@192.168.0.14>
2020-03-17 22:40:56 +0900

    ホームページ作成の練習課題

---
+++ haga_shop/src/main/java/com/example/demo/HagaShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagaShopApplication.class, args);
+	}
+
+}
